e0a9bcf1204f5de1fdbc0de6191b4fe9c74020b3
smorgrav <torbjorn@smorgrav.org>
2024-05-16 21:04:14 +0200

    Update github link and cleanup README

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ export default defineConfig({
         "nb-NO": "Velpr Online",
       },
       social: {
-        github: "https://github.com/withastro/starlight",
+        github: "https://github.com/smorgrav/velprdoc",
       },
       defaultLocale: "root",
       locales: {
